2a8d3603310f3338694b5972006a3b5ce1ccc5ab
Ronel <atilloronel14@gmail.com>
2025-11-13 22:32:24 +0800

    Modify multiple files and added a new file for login feature purposes

--- SpringCleanProject/SpringClean/SpringClean/src/main/java/com/project/SpringClean/controller/CustomerController.java
+++ SpringCleanProject/SpringClean/SpringClean/src/main/java/com/project/SpringClean/controller/CustomerController.java
@@ -1,17 +1,20 @@
 package com.project.SpringClean.controller;
 
+import com.project.SpringClean.dto.LoginRequest;
 import com.project.SpringClean.model.Customer;
 import com.project.SpringClean.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/customers")
-@CrossOrigin(origins = "http://localhost:3000") // adjust your frontend port
+@CrossOrigin(origins = "http://localhost:3000")
 public class CustomerController {
 
     @Autowired
@@ -25,5 +28,26 @@ public class CustomerController {
 
         customerRepository.save(customer);
         return ResponseEntity.ok(Map.of("message", "Customer registered successfully!"));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        Optional<Customer> customerOpt = customerRepository.findByEmail(request.getEmail());
+
+        if (customerOpt.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", "Email not found!"));
+        }
+
+        Customer customer = customerOpt.get();
+
+        // Compare password
+        if (!customer.getPassword().equals(request.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", "Incorrect password!"));
+        }
+
+        // Success
+        return ResponseEntity.ok(customer);
     }
 }
---
+++ SpringCleanProject/SpringClean/SpringClean/src/main/java/com/project/SpringClean/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.project.SpringClean.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginRequest {
+    private String email;
+    private String password;
+
+}
